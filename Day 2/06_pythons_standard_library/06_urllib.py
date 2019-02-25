6ae8370efbfe3a538b36f1a2aec534b1ae791bb4
daniel <daniel@daniel.com>
2019-02-25 03:25:02 +0200

    More updates

--- Day 2/06_pythons_standard_library/06_urllib.py
+++ Day 2/06_pythons_standard_library/06_urllib.py
@@ -6,3 +6,11 @@ http_response = urllib.request.urlopen("https://www.bankhapoalim.co.il/")
 html = http_response.read()
 
 print(str(html[:500], "utf-8"))
+
+
+# Extract domain name from an URL:
+from urllib.parse import urlparse
+
+parsed_url = urlparse('https://www.bankhapoalim.co.il/wps/portal/PoalimPrivate/branches')
+
+print(parsed_url.netloc)
